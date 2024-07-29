631d77fd709925e52d9bfd4b3f6f3c6b7bbc6120
muudar <mohammedmudher@hotmail.com>
2024-07-29 22:44:33 +0300

    changed home page text for hosting test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-green-600">
-      HOME
+      HOME PAGE
     </main>
   );
 }
