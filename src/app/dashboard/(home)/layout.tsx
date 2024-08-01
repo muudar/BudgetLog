9c47b0e98c5c3c0d17740a57db247add486c6d6d
muudar <mohammedmudher@hotmail.com>
2024-08-01 19:09:08 +0300

    added extra main class for layout

--- src/app/dashboard/(home)/layout.tsx
+++ src/app/dashboard/(home)/layout.tsx
@@ -88,7 +88,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
               </nav>
             </SheetContent>
           </Sheet>
-          <div>{children}</div>
+          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"></main>
         </header>
       </div>
     </div>
