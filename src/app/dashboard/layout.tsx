e0c1285fa43445266b58d74e1704de8569f2c489
muudar <mohammedmudher@hotmail.com>
2024-08-01 19:43:26 +0300

    removed (home) dir

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -117,7 +117,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
           </div>
           <UserButton></UserButton>
         </header>
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"></main>
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+          {children}
+        </main>
       </div>
     </div>
   );
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div className="flex flex-col"></div>;
+};
+
+export default page;
