e338dcce7b8f4d1b0215bc8de05f22b186f8e991
muudar <mohammedmudher@hotmail.com>
2024-08-13 20:49:38 +0300

    [feat] created transaction server action

--- src/actions/transactions.tsx
+++ src/actions/transactions.tsx
@@ -4,4 +4,52 @@ import prisma from '@/lib/db';
 import { auth } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 
-//TODO: addEarningsRecord
+//TODO: Make transaction for increasing balance with record
+//TODO: Create type for earning records
+type EarningRecordData = any;
+
+export async function addEarningsRecord(data: EarningRecordData) {
+  try {
+    const { userId } = auth();
+    if (!userId) throw new Error('User not authenticated');
+    if (!data) throw new Error('Missing record data');
+    let categoryId = null;
+    if (data.pickedCategory) {
+      let category = await prisma.category.findFirst({
+        where: {
+          userId: userId,
+          name: data.pickedCategory,
+        },
+      });
+      if (category) categoryId = category.id;
+    }
+    const earningRecord = await prisma.transaction.create({
+      data: {
+        userId: userId,
+        amount: data.amount,
+        type: 'EARNING',
+        description: data.description ?? '',
+        categoryId: categoryId ?? null,
+      },
+    });
+    if (earningRecord) {
+      return {
+        message: 'Earnings record created successfully',
+        ok: true,
+      };
+    } else {
+      return {
+        message: 'Failed to create earnings record',
+        ok: false,
+      };
+    }
+  } catch (err) {
+    console.error(err);
+    return {
+      message: err instanceof Error ? err.message : 'Internal Server Error',
+      ok: false,
+    };
+  } finally {
+    revalidatePath('/dashboard');
+  }
+}
--- src/app/(auth-dashboard)/dashboard/_components/cards/modals/EarningsModal.tsx
+++ src/app/(auth-dashboard)/dashboard/_components/cards/modals/EarningsModal.tsx
@@ -26,14 +26,21 @@ import {
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { ArrowUpRight } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { FormEvent, useCallback, useEffect, useState } from 'react';
 import { getCategories } from '@/actions/categories';
 import AddCategoryModal from './AddCategoryModal';
+import { Textarea } from '@/components/ui/textarea';
+import { set } from 'date-fns';
+import toast from 'react-hot-toast';
+import { addEarningsRecord } from '@/actions/transactions';
 
 type Props = {
   currentBalance: number;
   currentSavings: number;
 };
+
+// TODO: Create form data type for this form
+type FormData = any;
 
 //TODO: Create category type
 type Category = any;
@@ -41,10 +48,15 @@ type Category = any;
 //TODO: Add Server action for adding earning record
 //TODO: handle emoji for server action (create emoji state)
 const EarningsModal = ({ currentBalance, currentSavings }: Props) => {
-  const [categories, setCategories]: Category[] = useState([]);
-  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
-  const [amount, setAmount] = useState<number>(0);
   const [open, setOpen] = useState(false);
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+  const [categories, setCategories]: Category[] = useState([]);
+  const [data, setData] = useState<FormData>({
+    amount: 0,
+    pickedCategory: null,
+    description: '',
+  });
+  console.log(data);
   useEffect(() => {
     const fetchCategories = async () => {
       const res = await getCategories();
@@ -54,32 +66,32 @@ const EarningsModal = ({ currentBalance, currentSavings }: Props) => {
     };
     fetchCategories();
   }, [categoryModalOpen]);
-  //   const handleBalanceTransfer = useCallback(
-  //     async (e: FormEvent<HTMLFormElement>) => {
-  //       try {
-  //         toast.loading('Transferring...', {
-  //           id: 'loading',
-  //         });
-  //         e.preventDefault();
-  //         const res = await transferBalance(transferAmount, currentBalance);
-  //         if (res.ok) {
-  //           toast.success(res.message);
-  //           setOpen(false);
-  //         }
-  //         if (!res.ok) {
-  //           toast.error(res.message || 'Internal server error');
-  //         }
-  //       } catch (error) {
-  //         toast.error(
-  //           error instanceof Error ? error.message : 'Internal server error'
-  //         );
-  //         console.error(error);
-  //       } finally {
-  //         toast.remove('loading');
-  //       }
-  //     },
-  //     [transferAmount, currentSavings]
-  //   );
+  const handleSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      try {
+        toast.loading('Creating record', {
+          id: 'loading',
+        });
+        e.preventDefault();
+        const res = await addEarningsRecord(data);
+        if (res.ok) {
+          toast.success(res.message);
+          setOpen(false);
+        }
+        if (!res.ok) {
+          toast.error(res.message || 'Internal server error');
+        }
+      } catch (error) {
+        toast.error(
+          error instanceof Error ? error.message : 'Internal server error'
+        );
+        console.error(error);
+      } finally {
+        toast.remove('loading');
+      }
+    },
+    [data]
+  );
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -90,7 +102,7 @@ const EarningsModal = ({ currentBalance, currentSavings }: Props) => {
       </DialogTrigger>
       <DialogContent>
         <DialogTitle>Earnings</DialogTitle>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Card>
             <CardHeader>
               <CardTitle>Add Earnings Record</CardTitle>
@@ -112,12 +124,18 @@ const EarningsModal = ({ currentBalance, currentSavings }: Props) => {
                     type="number"
                     step="0.01"
                     min={0.01}
-                    onChange={(e) => setAmount(Number(e.target.value))}
+                    onChange={(e) =>
+                      setData({ ...data, amount: Number(e.target.value) })
+                    }
                   />
                 </div>
                 <div>
                   <Label htmlFor="transferAmount">Category</Label>
-                  <Select>
+                  <Select
+                    onValueChange={(e: string) =>
+                      setData({ ...data, pickedCategory: e })
+                    }
+                  >
                     <SelectTrigger>
                       <SelectValue placeholder="Select a category" />
                     </SelectTrigger>
@@ -128,7 +146,7 @@ const EarningsModal = ({ currentBalance, currentSavings }: Props) => {
                           categories.map((category: Category) => (
                             <SelectItem
                               key={category.name}
-                              value={category.emoji + ' ' + category.name}
+                              value={category.name}
                             >
                               {category.emoji + ' ' + category.name}
                             </SelectItem>
@@ -145,6 +163,15 @@ const EarningsModal = ({ currentBalance, currentSavings }: Props) => {
                       </SelectGroup>
                     </SelectContent>
                   </Select>
+                </div>
+                <div className="col-span-2 space-y-2">
+                  <Label htmlFor="transferAmount">Description</Label>
+                  <Textarea
+                    id="description"
+                    onChange={(e) =>
+                      setData({ ...data, description: e.target.value })
+                    }
+                  ></Textarea>
                 </div>
               </div>
             </CardContent>
