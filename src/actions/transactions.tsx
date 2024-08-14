793645b03721be5a951e8b0e6a4dabd3051fc5b7
muudar <mohammedmudher@hotmail.com>
2024-08-14 10:28:23 +0300

    [bugfix] added earnings increase in transaction

--- src/actions/transactions.tsx
+++ src/actions/transactions.tsx
@@ -38,6 +38,9 @@ export async function addEarningsRecord(data: EarningFormData) {
           balance: {
             increment: data.amount,
           },
+          earnings: {
+            increment: data.amount,
+          },
         },
       }),
     ]);
