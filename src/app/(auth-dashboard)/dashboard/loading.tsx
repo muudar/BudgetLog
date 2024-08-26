f73f39843d170b0edb1f562baca3f1bc5eebd40b
muudar <mohammedmudher@hotmail.com>
2024-08-26 20:05:50 +0300

    [bugfix] fix loaders skeleton height

--- src/app/(auth-dashboard)/dashboard/loading.tsx
+++ src/app/(auth-dashboard)/dashboard/loading.tsx
@@ -6,21 +6,21 @@ const loading = () => {
     <>
       <div className="grid grid-cols-2 grid-rows-2 gap-2 lg:col-span-6">
         <Skeleton
-          className={`h-[150px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
+          className={`h-[110px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
         ></Skeleton>
         <Skeleton
-          className={`h-[150px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
+          className={`h-[110px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
         ></Skeleton>
         <Skeleton
-          className={`h-[150px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
+          className={`h-[110px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
         ></Skeleton>
         <Skeleton
-          className={`h-[150px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
+          className={`h-[110px] w-full bg-slate-200 lg:h-[172px] xl:h-[200px]`}
         ></Skeleton>
       </div>
       <div className="lg:col-span-4">
         <Skeleton
-          className={`h-[335px] w-full bg-slate-200 lg:h-[350px] xl:h-[405px]`}
+          className={`h-[320px] w-full bg-slate-200 lg:h-[350px] xl:h-[405px]`}
         ></Skeleton>
       </div>
       <div className="lg:col-span-4">
@@ -30,10 +30,10 @@ const loading = () => {
       </div>
       <div className="flex flex-col justify-between gap-4 lg:col-span-6">
         <Skeleton
-          className={`h-[200px] w-full bg-slate-200 lg:h-[250px]`}
+          className={`h-[250px] w-full bg-slate-200 lg:h-[250px]`}
         ></Skeleton>
         <Skeleton
-          className={`h-[200px] w-full bg-slate-200 lg:h-[250px]`}
+          className={`h-[250px] w-full bg-slate-200 lg:h-[250px]`}
         ></Skeleton>
       </div>
     </>
