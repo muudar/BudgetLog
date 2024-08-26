aa0537edd6122de3fc86d373638bc81d77455757
muudar <mohammedmudher@hotmail.com>
2024-08-26 20:27:06 +0300

    [responsivity] improve landing page responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-muted/60 px-12 py-10">
+    <div className="flex min-h-screen flex-col items-center bg-muted/60 px-6 py-10 sm:px-12">
       <nav className="flex w-full items-center justify-between">
         <div className="text-xl font-bold">BudgetLog</div>
         <div className="flex items-center gap-8">
-          <div className="hidden items-center gap-8 sm:flex">
+          <div className="hidden items-center gap-8 md:flex">
             <div className="cursor-pointer">Features</div>
             <div className="cursor-pointer">Why BudgetLog?</div>
             <div className="cursor-pointer">Pricing</div>
@@ -21,25 +21,28 @@ export default function Home() {
         </div>
       </nav>
       <main className="mt-[50px] flex w-full flex-col justify-between gap-12 lg:flex-row">
-        <div className="flex flex-col gap-4">
+        <div className="flex w-screen flex-col items-center gap-4 self-center px-8 text-center lg:w-full lg:items-start lg:px-0 lg:text-left">
           <div className="text-5xl xl:text-7xl">
             Simple way to manage your money
           </div>
           <div className="text-slate-500">
             Log your spendings and earnings now!
           </div>
-          <a href="/sign-up">
+          <a href="/sign-up" className="w-[150px]">
             <Button className="w-[150px] gap-2 bg-black hover:bg-slate-600">
               Get Started <ArrowRight />
             </Button>
           </a>
         </div>
-        <Image
-          src="/devices.png"
-          width={800}
-          height={700}
-          alt="Image of device mockup"
-        />
+        <div>
+          <Image
+            src="/devices.png"
+            layout="responsive"
+            width={600}
+            height={0}
+            alt="Image of device mockup"
+          />
+        </div>
       </main>
     </div>
   );
